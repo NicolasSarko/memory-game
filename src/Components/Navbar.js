3bd777ffe1fafa92515f9cecc3710feeb0adcbca
NicolasSarko <48804214+NicolasSarko@users.noreply.github.com>
2020-12-22 17:40:52 +0100

    Update Navbar.js

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,9 +5,9 @@ class Navbar extends React.Component {
     const { name, score } = this.props;
     return (
       <div className="navbar">
-        <div className="game-title">Memory</div>
+        <div className="game-title">Memorygame</div>
         <div className="name">{name}</div>
-        <div className="score">Score: {score}</div>
+        <div className="score">Ton Score: {score}</div>
       </div>
     );
   }
